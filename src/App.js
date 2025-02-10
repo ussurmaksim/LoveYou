0a91f459f73a33f0ebaba697c12d485685378d95
ussurmaksim <94112489+ussurmaksim@users.noreply.github.com>
2025-02-10 21:13:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/header";
+import Section1 from "./components/section1";
+import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
+
+export default function App() {
+  return (
+    <div className="App">
+        <Header  />
+        <Section1/>
+        <Section2/>
+        <Section3/>
+    </div>
+  );
+}
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export  default class Header extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <a href="#">Люблю</a>
+                <a href="#">Тебя</a>
+                <a href="#">Очень</a>
+                <a href="#">Сильно</a>
+
+            </header>
+        )
+    }
+}
